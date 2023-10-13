f0c7381ce64cfc74a40c9fcbb88a0729028cce7f
Roman <americano.creem@gmail.com>
2023-10-13 12:39:05 +0500

    fix(route): basename fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
 
   return (
-    <Router basename={import.meta.env.DEV ? '/' : '/products-catalog/'}>
+    <Router basename={'/'}>
       <Header/>
       <Categories/>
       <Routes>
